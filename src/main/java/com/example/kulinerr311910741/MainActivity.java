d09143eca3da13c8a836ed07b58d3c5dc6291bdc
ewangdesta <81276952+ewangdesta123@users.noreply.github.com>
2022-07-11 15:05:53 +0700

    Initial commit

---
+++ src/main/java/com/example/kulinerr311910741/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kulinerr311910741;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
